6d78b587f97331a9f3d479371cefe2991dcc493a
0x1DOCD00D <markgrechanik@gmail.com>
2024-05-13 16:00:38 -0500

    5/13

--- src/main/java/HowdyYall.java
+++ src/main/java/HowdyYall.java
@@ -1,5 +1,12 @@
 public class HowdyYall {
     public static void main(String[] args) {
-        System.out.println("Welcome to 474!");
+        {
+            String courseName = "CS474";
+            {
+                System.out.println(courseName);
+            }
+        }
+//        System.out.println("Welcome to" + courseName);
+
     }
 }
---
+++ src/main/java/Null.java
@@ -0,0 +1,10 @@
+public class Null {
+    public static void greetMethod() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String[] args) {
+        ((Null) null).greetMethod();
+    }
+}
+
